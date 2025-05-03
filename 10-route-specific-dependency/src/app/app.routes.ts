994cd1295ecc70411c674f9c810e6f71953ec147
Rakesh KN <rakeshshandilyaa@gmail.com>
2025-05-03 22:03:33 +0530

    provided the dependency in the routes

--- 10-route-specific-dependency/src/app/app.routes.ts
+++ 10-route-specific-dependency/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
 import { NeighbourComponent } from './neighbour/neighbour.component';
 import { DetailComponent } from './detail/detail.component';
+import { SharedService } from './shared.service';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'parent', pathMatch: 'full' },
-    { path: 'parent', component: ParentComponent },
-    { path: 'neighbour', component: NeighbourComponent,
+    { path: 'parent', component: ParentComponent, providers: [SharedService] },
+    { path: 'neighbour', component: NeighbourComponent, providers: [SharedService],
         children: [
             { path: 'detail', component: DetailComponent }
         ]
